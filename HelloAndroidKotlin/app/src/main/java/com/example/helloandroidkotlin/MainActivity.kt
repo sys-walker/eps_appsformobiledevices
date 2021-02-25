e33127f28030eef9acb1534f4ba7bc4e0a5933ee
sys-walker <pere.rollon@gmail.com>
2021-02-25 11:30:43 +0100

    Add Hello Android in Kotlin

---
+++ HelloAndroidKotlin/app/src/main/java/com/example/helloandroidkotlin/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.helloandroidkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import android.view.Menu
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_hello_world,menu)
+        return true
+    }
+}
