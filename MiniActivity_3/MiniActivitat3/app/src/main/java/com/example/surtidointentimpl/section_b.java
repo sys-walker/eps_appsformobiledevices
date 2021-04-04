8cef11a3f2c3a2d5050b8ac70ca515b5b70d05df
sys-walker <pere.rollon@gmail.com>
2021-04-04 23:11:42 +0200

    folders orders miniactivity 3

---
+++ MiniActivity_3/MiniActivitat3/app/src/main/java/com/example/surtidointentimpl/section_b.java
@@ -0,0 +1,14 @@
+package com.example.surtidointentimpl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class section_b extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_b);
+    }
+}
